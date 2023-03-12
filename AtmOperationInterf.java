d955443a5e7260fd0aaef3261d801d1fc05fce40
Saksham-Kamble <127680181+Saksham-Kamble@users.noreply.github.com>
2023-03-12 22:36:40 +0530

    ATM Interface

---
+++ AtmOperationInterf.java
@@ -0,0 +1,4 @@
+
+
+public interface AtmOperationInterf { public void viewBalance(); public void withdrawAmount(double withdrawAmount); public void depositAmount(double depositAmount ); public void viewMiniStatement();
+}
